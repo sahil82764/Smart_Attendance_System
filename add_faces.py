33af2c133de4460ade9775ea557ef531aec2400e
Sahil Khan <95177044+sahil82764@users.noreply.github.com>
2023-12-13 17:23:13 -0500

    Add files via upload

--- add_faces.py
+++ add_faces.py
@@ -2,46 +2,53 @@ import cv2
 import pickle
 import numpy as np
 import os
-video=cv2.VideoCapture(0)
-facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-faces_data=[]
+video = cv2.VideoCapture(0)
+facedetect = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-i=0
+faces_data = []
+capturing_frequency = 2  # Capture every 2 frames
 
-name=input("Enter Your Name: ")
+i = 0
+
+name = input("Enter Your Name: ")
 
 while True:
-    ret,frame=video.read()
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces=facedetect.detectMultiScale(gray, 1.3 ,5)
-    for (x,y,w,h) in faces:
-        crop_img=frame[y:y+h, x:x+w, :]
-        resized_img=cv2.resize(crop_img, (50,50))
-        if len(faces_data)<=100 and i%10==0:
+    ret, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facedetect.detectMultiScale(gray, 1.3, 5)
+    
+    for (x, y, w, h) in faces:
+        crop_img = frame[y:y+h, x:x+w, :]
+        resized_img = cv2.resize(crop_img, (50, 50))
+        
+        if len(faces_data) < 50 and i % capturing_frequency == 0:
             faces_data.append(resized_img)
-        i=i+1
-        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 1)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
-    cv2.imshow("Frame",frame)
-    k=cv2.waitKey(1)
-    if k==ord('q') or len(faces_data)==100:
+        
+        i += 1
+        cv2.putText(frame, str(len(faces_data)), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 255), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 1)
+    
+    cv2.imshow("Frame", frame)
+    k = cv2.waitKey(1)
+    
+    if k == ord('q') or len(faces_data) == 50:
         break
+
 video.release()
 cv2.destroyAllWindows()
 
-faces_data=np.asarray(faces_data)
-faces_data=faces_data.reshape(100, -1)
-
+faces_data = np.asarray(faces_data)
+faces_data = faces_data.reshape(len(faces_data), -1)
 
 if 'names.pkl' not in os.listdir('data/'):
-    names=[name]*100
+    names = [name] * len(faces_data)
     with open('data/names.pkl', 'wb') as f:
         pickle.dump(names, f)
 else:
     with open('data/names.pkl', 'rb') as f:
-        names=pickle.load(f)
-    names=names+[name]*100
+        names = pickle.load(f)
+    names = names + [name] * len(faces_data)
     with open('data/names.pkl', 'wb') as f:
         pickle.dump(names, f)
 
@@ -50,7 +57,7 @@ if 'faces_data.pkl' not in os.listdir('data/'):
         pickle.dump(faces_data, f)
 else:
     with open('data/faces_data.pkl', 'rb') as f:
-        faces=pickle.load(f)
-    faces=np.append(faces, faces_data, axis=0)
+        faces = pickle.load(f)
+    faces = np.append(faces, faces_data, axis=0)
     with open('data/faces_data.pkl', 'wb') as f:
         pickle.dump(faces, f)
